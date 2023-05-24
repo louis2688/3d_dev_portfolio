30c039a92d518f416fbd893e8b8ef47aa593b013
louis2688 <louismadrigal26@gmail.com>
2023-05-24 17:41:22 +0800

    Add earth component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -71,10 +71,10 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText} text-center`}>Projects.</h2>
       </motion.div>
 
-      <div className="w-full flex ">
+      <div className="w-full flex">
         <motion.p
           variants={fadeIn('', '', 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt-3 text-secondary text-[17px] max-w-5xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
