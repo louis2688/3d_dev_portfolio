de48cab1d1dbe30e072b92b72faf61eee88c3fb1
louis2688 <louismadrigal26@gmail.com>
2023-05-27 12:37:34 +0800

    Update github link

--- src/constants/index.js
+++ src/constants/index.js
@@ -429,7 +429,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+      'I thought it was impossible to make a website as beautiful as our product, but Louis proved me wrong.',
     name: 'Sara Lee',
     designation: 'CFO',
     company: 'Acme Co',
@@ -437,7 +437,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      "I've never met a web developer who truly cares about their clients' success like Louis does.",
     name: 'Chris Brown',
     designation: 'COO',
     company: 'DEF Corp',
@@ -445,7 +445,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      "After Louis optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: 'Lisa Wang',
     designation: 'CTO',
     company: '456 Enterprises',
@@ -473,7 +473,7 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: 'https://github.com/',
+    source_code_link: 'https://github.com/louis2688',
   },
   {
     name: 'Job IT',
@@ -494,7 +494,7 @@ const projects = [
       },
     ],
     image: jobit,
-    source_code_link: 'https://github.com/',
+    source_code_link: 'https://github.com/louis2688',
   },
   {
     name: 'Trip Guide',
@@ -515,7 +515,7 @@ const projects = [
       },
     ],
     image: tripguide,
-    source_code_link: 'https://github.com/',
+    source_code_link: 'https://github.com/louis2688',
   },
 ];
 
